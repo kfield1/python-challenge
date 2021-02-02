b39bff212bd23767ae30a9eb6b4c88683c380837
kfield1 <kayt1303@yahoo.com>
2021-02-02 11:51:32 -0600

    added project

---
+++ PyBank/main.py
@@ -0,0 +1,15 @@
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" over the entire period
+# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
+
+
+# results should look like
+# Financial Analysis
+# ----------------------------
+# Total Months: 86
+# Total: $38382578
+# Average  Change: $-2315.12
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($-2196167)
