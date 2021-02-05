cf1df046be2518b04d129361efd0c3fdb3cd3074
kfield1 <kayt1303@yahoo.com>
2021-02-04 18:28:11 -0600

    fixed problem

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,10 @@
 
-total_months =0
-total_profit_loss_amount =0.00
-average_profit_loss = 0.00
-greatest_increase = {"date":"", "amount": 0}
-greatest_decrease = {"date":"", "amount": 0}
 import csv
+total_months = 0
+total_profit_loss_amount = 0.00
+average_profit_loss = 0.00
+greatest_increase = {"date": "", "amount": 0}
+greatest_decrease = {"date": "", "amount": 0}
 file_path = "./Resources/budget_data.csv"
 with open(file_path) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
@@ -15,28 +15,30 @@ with open(file_path) as csvfile:
     # Read each row of data after the header
     for row in csvreader:
         # The total number of months included in the dataset
-         total_months = total_months + 1
-         date = row[0]
-         profit = float(row[1])
+        total_months = total_months + 1
+        date = row[0]
+        profit = float(row[1])
         # The net total amount of "Profit/Losses" over the entire period
-        
+
         # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
         # The greatest increase in profits (date and amount) over the entire period
-        # if (profit > greatest_increase["amount"]):
-            # greatest_increase["date"] = date
-            # greatest_increase["amount"] = profit
+        if (profit > greatest_increase["amount"]):
+            greatest_increase["date"] = date
+            greatest_increase["amount"] = profit
         # The greatest decrease in losses (date and amount) over the entire period
         if (profit < greatest_decrease["amount"]):
             greatest_decrease["date"] = date
             greatest_decrease["amount"] = profit
-        
+
 
 # print results
 print("Financial Analysis")
 print("---------------------")
 print(f"Total Months: {total_months}")
-print(f"Greatest Increase in Profits: {greatest_increase['date']} (${greatest_increase['amount']})")
-print(f"Greatest Decrease in Profits: {greatest_decrease['date']} (${greatest_decrease['amount']})")
+print(
+    f"Greatest Increase in Profits: {greatest_increase['date']} (${greatest_increase['amount']})")
+print(
+    f"Greatest Decrease in Profits: {greatest_decrease['date']} (${greatest_decrease['amount']})")
 # results should look like
 # Financial Analysis
 # ----------------------------
