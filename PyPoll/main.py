5a48bb53846369195427a25352949c9294c4b99e
kfield1 <kayt1303@yahoo.com>
2021-02-08 11:11:40 -0600

    adding print

---
+++ PyPoll/main.py
@@ -0,0 +1,51 @@
+total_votes = 0
+
+
+import csv
+file_path = "./Resources/election_data.csv"
+with open(file_path) as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile)
+    # Read the header row first(skip this step if there is no header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    # Read each row of data after the header
+    for row in csvreader:
+        # The total number of votes cast
+        total_votes = total_votes + 1
+
+        # A complete list of candidates who received votes
+
+
+        # The percentage of votes each candidate won
+
+
+        # The total number of votes each candidate won
+
+
+        # The winner of the election based on popular vote.
+
+# print results
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+
+
+
+# What it should look like
+# Election Results
+# -------------------------
+# Total Votes: 3521001
+# -------------------------
+# Khan: 63.000% (2218231)
+# Correy: 20.000% (704200)
+# Li: 14.000% (492940)
+# O'Tooley: 3.000% (105630)
+# -------------------------
+# Winner: Khan
+# -------------------------
+
+
+
+
